0566bb5c08e1dd4e5a05e2936a255147e3da68d9
Janik Schumacher <rndmb0t@gmail.com>
2020-07-05 21:17:09 +0200

    init method refactoring (sonar qube)

--- cli/src/controller/init.controller.ts
+++ cli/src/controller/init.controller.ts
@@ -15,128 +15,48 @@ export class InitController extends Controller {
       throw new Error('A module cannot be declared as host and entry module');
     }
 
-    const alreadyInitialized = this._configService.gahConfigExists();
-
-    const overwriteHost = await this._promptService
-      .confirm({
-        msg: 'This folder already contains a GAH configuration. A host has to be in its own workspace. Do you want to overwrite the existing configuration for this workspace?',
-        enabled: () => {
-          return (isHost ?? false) && alreadyInitialized;
-        }
-      });
+    const { alreadyInitialized, overwriteHost } = await this.askForOverwrite(isHost);
     if ((isHost ?? false) && alreadyInitialized && !overwriteHost) {
       return;
     }
 
-    let guessedModuleName: string = '';
-    const packageJson = this._fileSystemService.tryReadFile('package.json');
-    if (packageJson) {
-      const pkgJson = JSON.parse(packageJson) as PackageJson;
-      if (pkgJson.name) {
-        const pkgName = pkgJson.name.match(/(@[\w-]+\/)?([\w/\-.]+)/)?.[2]?.replace(/\//, '-').toLocaleLowerCase();
-        if (pkgName) {
-          guessedModuleName = pkgName;
-        }
-      }
-    }
-    const newModuleName = await this._promptService
-      .input({
-        msg: 'Enter a unique name for this ' + (isHost ? 'host' : 'module'),
-        enabled: () => !isHost,
-        default: guessedModuleName
-      });
+    const packageJson = this.getPackageJson();
+
+    const newModuleName = await this.askModuleName(isHost, packageJson);
     if (!newModuleName && !isHost) {
       this._loggerService.warn('No module name provided...');
       return;
     }
 
-    let guessedPackageName: string = '';
-    if (packageJson) {
-      const pkgJson = JSON.parse(packageJson) as PackageJson;
-      if (pkgJson.name) {
-        const pkgName = pkgJson.name.match(/@([\w-]+)\//)?.[1].toLocaleLowerCase();
-        if (pkgName) {
-          guessedPackageName = pkgName;
-        }
-      }
-    }
-
-    const packageName = await this._promptService
-      .input({
-        msg: 'Enter the name of the package prefix of this module',
-        enabled: () => !isHost,
-        default: guessedPackageName || null
-      });
+    const packageName = await this.askPackageName(packageJson, isHost);
     if (!packageName && !isHost) {
       this._loggerService.warn('No package name provided...');
       return;
     }
 
-    const overwrite = await this._promptService
-      .confirm({
-        msg: 'A module with this name has already been added to this workspace, do you want to overwrite it?',
-        enabled: () => {
-          this.doesNameExist(this._configService.getGahModule(), newModuleName!);
-          return this.nameExists;
-        }
-      });
+    const overwrite = await this.askForModuleOverwrite(newModuleName);
     if (this.nameExists && !overwrite) {
       return;
     }
 
 
-    const hasFacadeFolderPath = await this._promptService
-      .confirm({
-        msg: 'Does this module contain a folder for facade files?',
-        enabled: () => !isHost,
-      });
-
-    let defaultFacadePath: string | null = null;
-    if (hasFacadeFolderPath) {
-      defaultFacadePath = this._fileSystemService.getFilesFromGlob('**/facade', ['.gah', 'dist'])?.[0];
-
-      if (process.platform === 'win32') {
-        defaultFacadePath = defaultFacadePath?.replace(/\//g, '\\');
-      }
-    }
-
-    const facadeFolderPath = await this._promptService
-      .fuzzyPath({
-        msg: 'Enter the path to the folder containing the facade files',
-        enabled: () => !isHost && hasFacadeFolderPath,
-        itemType: 'directory',
-        excludePattern: ['.gah', 'dist'],
-        default: defaultFacadePath
-      });
+    const facadeFolderPath = await this.askForFacadeFolderPath(isHost);
 
     let defaultPublicApiPath = this._fileSystemService.getFilesFromGlob('**/public-api.ts', ['.gah', 'dist'])?.[0]
       ?? this._fileSystemService.getFilesFromGlob('**/index.ts', ['.gah', 'dist'])?.[0];
 
-    if (process.platform === 'win32') {
-      defaultPublicApiPath = defaultPublicApiPath.replace(/\//g, '\\');
-    }
-
-    const publicApiPath = await this._promptService
-      .fuzzyPath({
-        msg: 'Enter the path to the public-api file (public-api.ts / index.ts / ...)',
-        enabled: () => !isHost,
-        itemType: 'file',
-        exclude: (val) => !val.endsWith('.ts') || val.endsWith('.d.ts'),
-        excludePattern: ['.gah'],
-        default: defaultPublicApiPath
-      });
+    const publicApiPath = await this.askForPublicApiPath(defaultPublicApiPath, isHost);
 
     if (!publicApiPath && !isHost) {
       this._loggerService.warn('No public-api path provided...');
       return;
     }
 
-    const baseModuleName = await this._promptService
-      .input({
-        msg: 'Enter the class name of the base NgModule for this GahModule (empty if there is none)',
-        enabled: () => !isHost,
-        default: this.tryGuessbaseModuleName()
-      });
+    this.doInit(isHost ?? false, newModuleName, facadeFolderPath, packageName, publicApiPath, isEntry ?? false, overwrite);
+  }
+
+  private async doInit(isHost: boolean, newModuleName: string, facadeFolderPath: string, packageName: string, publicApiPath: string, isEntry: boolean, overwrite: boolean) {
+    const baseModuleName = await this.askBaseModuleName(isHost);
 
     const newModule = new ModuleDefinition();
 
@@ -171,6 +91,127 @@ export class InitController extends Controller {
     this._configService.saveGahModuleConfig();
   }
 
+  private async askBaseModuleName(isHost: boolean | undefined) {
+    return await this._promptService
+      .input({
+        msg: 'Enter the class name of the base NgModule for this GahModule (empty if there is none)',
+        enabled: () => !isHost,
+        default: this.tryGuessbaseModuleName()
+      });
+  }
+
+  private getPackageJson() {
+    const packageJson = this._fileSystemService.tryReadFile('package.json');
+    if (packageJson) {
+      return JSON.parse(packageJson) as PackageJson;
+    }
+    return undefined;
+  }
+
+  private async askForPublicApiPath(defaultPublicApiPath: string, isHost: boolean | undefined) {
+    if (process.platform === 'win32') {
+      defaultPublicApiPath = defaultPublicApiPath.replace(/\//g, '\\');
+    }
+
+    const publicApiPath = await this._promptService
+      .fuzzyPath({
+        msg: 'Enter the path to the public-api file (public-api.ts / index.ts / ...)',
+        enabled: () => !isHost,
+        itemType: 'file',
+        exclude: (val) => !val.endsWith('.ts') || val.endsWith('.d.ts'),
+        excludePattern: ['.gah'],
+        default: defaultPublicApiPath
+      });
+    return publicApiPath;
+  }
+
+  private async askForFacadeFolderPath(isHost: boolean | undefined) {
+    const hasFacadeFolderPath = await this._promptService
+      .confirm({
+        msg: 'Does this module contain a folder for facade files?',
+        enabled: () => !isHost,
+      });
+
+    let defaultFacadePath: string | null = null;
+    if (hasFacadeFolderPath) {
+      defaultFacadePath = this._fileSystemService.getFilesFromGlob('**/facade', ['.gah', 'dist'])?.[0];
+
+      if (process.platform === 'win32') {
+        defaultFacadePath = defaultFacadePath?.replace(/\//g, '\\');
+      }
+    }
+
+    const facadeFolderPath = await this._promptService
+      .fuzzyPath({
+        msg: 'Enter the path to the folder containing the facade files',
+        enabled: () => !isHost && hasFacadeFolderPath,
+        itemType: 'directory',
+        excludePattern: ['.gah', 'dist'],
+        default: defaultFacadePath
+      });
+    return facadeFolderPath;
+  }
+
+  private async askForModuleOverwrite(newModuleName: string) {
+    return await this._promptService
+      .confirm({
+        msg: 'A module with this name has already been added to this workspace, do you want to overwrite it?',
+        enabled: () => {
+          this.doesNameExist(this._configService.getGahModule(), newModuleName!);
+          return this.nameExists;
+        }
+      });
+  }
+
+  private async askPackageName(packageJson?: PackageJson, isHost?: boolean) {
+    let guessedPackageName = '';
+    if (packageJson?.name) {
+      const pkgName = packageJson.name.match(/@([\w-]+)\//)?.[1].toLocaleLowerCase();
+      if (pkgName) {
+        guessedPackageName = pkgName;
+      }
+    }
+
+    const packageName = await this._promptService
+      .input({
+        msg: 'Enter the name of the package prefix of this module',
+        enabled: () => !isHost,
+        default: guessedPackageName || null
+      });
+    return packageName;
+  }
+
+  private async askModuleName(isHost?: boolean, packageJson?: PackageJson) {
+    let guessedModuleName: string = '';
+    if (packageJson?.name) {
+      const pkgName = packageJson.name.match(/(@[\w-]+\/)?([\w/\-.]+)/)?.[2]?.replace(/\//, '-').toLocaleLowerCase();
+      if (pkgName) {
+        guessedModuleName = pkgName;
+      }
+    }
+
+    const newModuleName = await this._promptService
+      .input({
+        msg: 'Enter a unique name for this ' + (isHost ? 'host' : 'module'),
+        enabled: () => !isHost,
+        default: guessedModuleName
+      });
+    return newModuleName;
+  }
+
+  private async askForOverwrite(isHost: boolean | undefined) {
+    const alreadyInitialized = this._configService.gahConfigExists();
+
+    const overwriteHost = await this._promptService
+      .confirm({
+        msg: 'This folder already contains a GAH configuration. A host has to be in its own workspace. Do you want to overwrite the existing configuration for this workspace?',
+        enabled: () => {
+          return (isHost ?? false) && alreadyInitialized;
+        }
+      });
+    return { alreadyInitialized, overwriteHost };
+  }
+
   private doesNameExist(cfg: GahModule, newName: string) {
     this.nameExists = cfg.modules.some(x => x.name === newName);
     return this.nameExists;
@@ -178,7 +219,9 @@ export class InitController extends Controller {
 
   private tryGuessbaseModuleName(): string | undefined {
     const possibleModuleFiles = this._fileSystemService.getFilesFromGlob('projects/**/src/lib/!(*routing*).module.ts');
-    if (!possibleModuleFiles || possibleModuleFiles.length === 0) { return undefined; }
+    if (!possibleModuleFiles || possibleModuleFiles.length === 0) {
+      return undefined;
+    }
 
     const firtsPossibleModuleContent = this._fileSystemService.readFile(possibleModuleFiles[0]);
     const match = firtsPossibleModuleContent.match(/export class (\S+) {/);
