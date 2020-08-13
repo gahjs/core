a9cfdb36783c13a2dd233ca6404a023a870258ec
LoaderB0T <rndmb0t@gmail.com>
2020-08-13 11:16:40 +0200

    global gah data service methods

--- shared/src/services/workspace.service.ts
+++ shared/src/services/workspace.service.ts
@@ -1,4 +1,8 @@
+import { GlobalGahData } from "../models/global-gah-data";
+
 export interface IWorkspaceService {
   ensureGitIgnoreLine(gitIgnorePattern: string, description?: string, cwd?: string): void;
   getGlobalGahFolder(): string;
+  getGlobalData(): GlobalGahData;
+  saveGlobalGahData(data: GlobalGahData): void;
 }
