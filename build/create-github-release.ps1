cf947459047d41f6561d7861a3e46c5a6492cbca
LoaderB0T <rndmb0t@gmail.com>
2020-08-14 22:14:31 +0200

    environment vars

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -12,8 +12,8 @@ $releaseTemplate = Get-Content -Raw -Path ./build/release-template.txt
 $releaseTemplate = $releaseTemplate.Replace('{{tag}}', $newCliVersion).Replace('{{date}}', $date);
 $releaseTemplate | Out-File ./build/release-template.txt -Encoding utf8
 
-$env:GITHUB_TOKEN = "$(GITHUB_TOKEN)"
-$env:GITHUB_USER = "$(GITHUB_USER)"
+$env:GITHUB_TOKEN = "$env:GITHUB_TOKEN"
+$env:GITHUB_USER = "$env:GITHUB_USER"
 
 Set-Location gah
 hub release create $newCliVersion --draft --commitish $sourceVersion --file release-template.txt
