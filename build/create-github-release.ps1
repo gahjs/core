2f445f7217d3324e36bf1195966842890330bc1e
LoaderB0T <rndmb0t@gmail.com>
2020-08-14 22:21:43 +0200

    CWD fixes

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -2,7 +2,6 @@ $newCliVersion = "v$env:releasescript_newCliVersion"
 $sourceVersion = "$env:Build_SourceVersion"
 $date = Get-Date -Format "MM/dd/yyyy"
 
-git clone https://github.com/awdware/gah.git
 
 brew install hub
 
@@ -15,5 +14,8 @@ $releaseTemplate | Out-File ./build/release-template.txt -Encoding utf8
 $env:GITHUB_TOKEN = "$env:GITHUB_TOKEN"
 $env:GITHUB_USER = "$env:GITHUB_USER"
 
+New-Item -ItemType Directory -Path gitfolder
+Set-Location gitfolder
+git clone https://github.com/awdware/gah.git
 Set-Location gah
-hub release create $newCliVersion --draft --commitish $sourceVersion --file release-template.txt
+hub release create $newCliVersion --draft --commitish $sourceVersion --file ../../build/release-template.txt
