ea250640852c849abffdaef1df027526da94dd54
LoaderB0T <rndmb0t@gmail.com>
2020-08-15 15:19:42 +0200

    removed token from file, duh.

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -1,22 +1,15 @@
-$newCliVersion = "v0.1.3"
-$newSharedVersion = "v0.1.0"
-$sourceVersion = "5d8f1a76753a6fe9c42ecee3c5f8e7e075504715"
-
-# $newCliVersion = "v$env:releasescript_newCliVersion"
-# $newCliVersion = "v$env:releasescript_newSharedVersion"
-# $sourceVersion = "$env:Build_SourceVersion"
+$newCliVersion = "v$env:releasescript_newCliVersion"
+$newCliVersion = "v$env:releasescript_newSharedVersion"
+$sourceVersion = "$env:Build_SourceVersion"
 $now = Get-Date -Format "MM\/dd\/yyyy"
 
 $releaseTemplate = Get-Content -Raw -Path release-template.txt
 
 $releaseTemplate = $releaseTemplate.Replace('{{tag}}', $newCliVersion).Replace('{{date}}', $now).Replace('{{tag-shared}}', $newSharedVersion)
 
-$env:GITHUB_TOKEN = "c4d44ad8d969ec14ac6079901d4664a742f766ce";
-
 $postHeaders = @{
   Authorization = "token $env:GITHUB_TOKEN"; 
 }
-
 
 $postParams = @{
   tag_name         = "$newCliVersion";
