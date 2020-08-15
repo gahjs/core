295885505816a0b6a450e2ad7ad322782d2f7400
LoaderB0T <rndmb0t@gmail.com>
2020-08-15 13:01:17 +0200

    github release script

--- build/create-github-release.ps1
+++ build/create-github-release.ps1
@@ -1,21 +1,21 @@
 $newCliVersion = "v$env:releasescript_newCliVersion"
+$newCliVersion = "v$env:releasescript_newSharedVersion"
 $sourceVersion = "$env:Build_SourceVersion"
-$date = Get-Date -Format "MM/dd/yyyy"
+$now = Get-Date -Format "MM\/dd\/yyyy"
 
+Write-Host $now
 
 brew install hub
 
+git clone https://github.com/awdware/gah.git
+Set-Location gah
 
-$releaseTemplate = Get-Content -Raw -Path ./build/release-template.txt
+$releaseTemplate = Get-Content -Raw -Path .\build\release-template.txt
 
-$releaseTemplate = $releaseTemplate.Replace('{{tag}}', $newCliVersion).Replace('{{date}}', $date);
-$releaseTemplate | Out-File ./build/release-template.txt -Encoding utf8
+$releaseTemplate = $releaseTemplate.Replace('{{tag}}', $newCliVersion).Replace('{{date}}', $now).Replace('{{tag-shared}}', $newSharedVersion);
+$releaseTemplate | Out-File .\build\release-template.txt -Encoding utf8
 
 $env:GITHUB_TOKEN = "$env:GITHUB_TOKEN"
 $env:GITHUB_USER = "$env:GITHUB_USER"
 
-New-Item -ItemType Directory -Path gitfolder
-Set-Location gitfolder
-git clone https://github.com/awdware/gah.git
-Set-Location gah
-hub release create $newCliVersion --draft --commitish $sourceVersion --file ../../build/release-template.txt
+hub release create $newCliVersion --draft --commitish $sourceVersion --file .\build\release-template.txt
