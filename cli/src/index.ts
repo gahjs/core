2031c0d9f103fd82338b0f226fbcfc4ad8cb6a3a
Janik Schumacher <rndmb0t@gmail.com>
2021-01-15 15:10:47 +0100

    exporting main entry point (async)

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -7,28 +7,33 @@ import chalk from 'chalk';
 import path from 'path';
 import { ContextService } from './services/context-service';
 
-const pjson = require(path.join(__dirname, '../package.json'));
-console.log(chalk.bold(chalk.whiteBright(`gah v${pjson.version}`)));
-let mainController: MainController;
-(async () => {
-  DIContainer.load();
-  mainController = DIContainer.resolve<MainController>(MainController);
+export const gahIndex = async () => {
 
-  DIContainer.get(ContextService).setContext({ calledFromCli: true });
+  const pjson = require(path.join(__dirname, '../package.json'));
+  console.log(chalk.bold(chalk.whiteBright(`gah v${pjson.version}`)));
+  let mainController: MainController;
+  await (async () => {
+    DIContainer.load();
+    mainController = DIContainer.resolve<MainController>(MainController);
 
-  // Call main method
-  await mainController.main();
+    DIContainer.get(ContextService).setContext({ calledFromCli: true });
 
-})().catch(err => {
-  console.log();
-  console.error(chalk.red(' ■ ') + err.message);
-  if (mainController['_contextService'].getContext().debug) {
-    console.log(err.stack);
-  }
-  try {
-    mainController['_cleanupService'].cleanJsonFileTemporaryChanges();
-  } catch (error) {
-    console.error(`${chalk.red(' ■ ')}Error during json file cleanup. Please discard unwanted changes manually`);
-  }
-  process.exit(1);
-});
+    // Call main method
+    await mainController.main();
+
+  })().catch(err => {
+    console.log();
+    console.error(chalk.red(' ■ ') + err.message);
+    if (mainController['_contextService'].getContext().debug) {
+      console.log(err.stack);
+    }
+    try {
+      mainController['_cleanupService'].cleanJsonFileTemporaryChanges();
+    } catch (error) {
+      console.error(`${chalk.red(' ■ ')}Error during json file cleanup. Please discard unwanted changes manually`);
+    }
+    process.exit(1);
+  });
+};
+
+gahIndex();
