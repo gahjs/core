3df6bf54d4782885343ba68245788b7c58425edb
Janik Schumacher <rndmb0t@gmail.com>
2021-01-15 21:52:04 +0100

    moved binary info

--- cli/src/gah-main.ts
+++ cli/src/gah-main.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import 'reflect-metadata';
 import DIContainer from './di-container';
 
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { gahMain } from './gah-main';
 
 gahMain();
