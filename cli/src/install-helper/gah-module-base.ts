985ada89aad56801386ba4a820b43ed78a8e0205
Janik Schumacher <janik.schumacher@helpline.pmcs.de>
2022-03-25 13:48:39 +0100

    Fixed lockfile paths

--- cli/src/install-helper/gah-module-base.ts
+++ cli/src/install-helper/gah-module-base.ts
@@ -576,10 +576,11 @@ export abstract class GahModuleBase {
       return InstallUnitResult.skipped;
     }
 
-    const yarnLockPath = this.fileSystemService.join(this.basePath, '..', 'yarn.lock');
+    const yarnLockDirectory = this.fileSystemService.join(this.basePath, '..');
+    const yarnLockPath = this.fileSystemService.join(yarnLockDirectory, 'yarn.lock');
     if (this.contextService.getContext().calledFromHostFolder && (await this.fileSystemService.fileExists(yarnLockPath))) {
       this.loggerService.log('Copying lockfile');
-      await this.fileSystemService.copyFile(yarnLockPath, this.fileSystemService.join(this.basePath, 'yarn.lock'));
+      await this.fileSystemService.copyFile(yarnLockPath, this.basePath);
     }
 
     this.loggerService.log('Installing yarn packages');
@@ -599,7 +600,7 @@ export abstract class GahModuleBase {
       this.loggerService.success('Packages installed successfully');
       if (this.contextService.getContext().calledFromHostFolder) {
         this.loggerService.log('Saving lockfile');
-        await this.fileSystemService.copyFile(this.fileSystemService.join(this.basePath, 'yarn.lock'), yarnLockPath);
+        await this.fileSystemService.copyFile(this.fileSystemService.join(this.basePath, 'yarn.lock'), yarnLockDirectory);
       }
     } else {
       this.loggerService.error(this.executionService.executionErrorResult);
